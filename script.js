662c1c280f7596e8a2584b9ec43f35c39ce02ecd
shannabenammar <55299429+shannabenammar@users.noreply.github.com>
2020-05-01 23:14:44 -0400

    TODO(4): Use replace function to ignore extra - character
    This is the final version I have for the javascript file.

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-
-
-function myKeyPress(e){
-
-  /* TODO: retrieve the value from the text input */
+function myKeyPress(e) {
+  
   /* TODO1: retrieve the value from the text input */
   var inputNum = document.getElementById("textinput").value;
   var mytextbox = "my textbox element";
-
-
-  // TODO: set the value of the textbox with the formatted value
-  // TODO2: set the value of the textbox with the formatted value
-  document.getElementById("textinput").value = formatPhoneNumber(inputNum);
-  
+  var count = 0; // for 3
   var keyPressed;
-  if(window.event) { // IE
+  
+  if (window.event) {
+    // IE
     keyPressed = e.keyCode;
-  } else if(e.which){ // Netscape/Firefox/Opera
+  } else if (e.which) {
+    // Netscape/Firefox/Opera
     keyPressed = e.which;
   }
 
-  var x = String.fromCharCode(keyPressed);
-  var y = formatPhoneNumber("7189515000");
+  var fixedNum = formatPhoneNumber(inputNum);
 
 
-  console.log("Key Pressed = " + x);
-  console.log("  Formatted = " + y);
-
-
-
-  
-
+  // TODO2: set the value of the textbox with the formatted value
+  document.getElementById("textinput").value = formatPhoneNumber(inputNum);
 }
 
-function formatPhoneNumber(value){
+function formatPhoneNumber(value) {
+ 
+  /* TODO4:  Use replace function to ignore extra - character */
+  //This TODO is also completed using the onlyNumbers() function below
+  value = value.replace(/\D/g, "");
+  value = value.replace("-", "");
+  value = value.replace("--", "");
 
-  /* TODO:  Use replace function to ignore extra - character */
-  
-  // TODO: Add a condition to ignore entries beyond 10 digits
   // TODO3: Add a condition to ignore entries beyond 10 digits
   //This TODO is also found in the html tag
   if (value.length > 10) 
     value = value.slice(0, 10);
-  if(value.length > 3 && value.length <= 6)
-    value = value.slice(0,3) + "-" + value.slice(3);
-  else if(value.length > 6)
-    value = value.slice(0,3) + "-" + value.slice(3,6) + "-" + value.slice(6);
+  if (value.length > 3 && value.length <= 6)
+    value = value.slice(0, 3) + "-" + value.slice(3);
+  else if (value.length > 6)
+    value = value.slice(0, 3) + "-" + value.slice(3, 6) + "-" + value.slice(6);
 
   return value;
+}
+  /* TODO4:  Use replace function to ignore extra - character */
+  //This TODO is also completed using the formatPhoneNumber(value) function above
+function onlyNumbers(value) {
+  
+  //ensures that the only values entered are numbers by using the numeric keycodes of the keyboard
+
+  var keyCode = value.keyCode || value.which;
+
+  const isNumeric =
+    (keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105);
+
+  return isNumeric;
 }
